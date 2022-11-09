686f904b163b82fe7546a67edff1e18633e50da7
adilet312 <adiletm@hotmail.com>
2022-11-08 18:46:03 -0800

    Modified flatten function.

--- flattenArray.js
+++ flattenArray.js
@@ -1,3 +1,3 @@
-const flatten = (arr,depth) => arr.reduce( (acc, item) =>  depth!==0 && Array.isArray(item) ? acc.concat(...flatten(item,depth-1)) : [...acc,item],[]);
+const flatten = (arr,depth) => arr.reduce( (acc,item) => Array.isArray(item) && depth > 1 ? acc.concat(...flatten(item, depth-1)) : acc.concat(item),[])
 
 console.log(flatten([2,3,[4,5,6,[3,1,[7,1]]]],1))
